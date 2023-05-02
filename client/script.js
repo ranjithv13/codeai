072bde34955cf9761fe09c49d6f14c9cdb3984bd
ranjithv13 <115389065+ranjithv13@users.noreply.github.com>
2023-05-02 17:35:19 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv)
 
-    const response = await fetch('https://codeai-amber.vercel.app', {
+    const response = await fetch('http://localhost:5000', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
