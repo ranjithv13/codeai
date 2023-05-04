cc45c492cbc67423e1a5367a3576475b4b30eca3
ranjithv13 <115389065+ranjithv13@users.noreply.github.com>
2023-05-04 23:22:00 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -10,10 +10,10 @@ function loader(element) {
     element.textContent = ''
 
     loadInterval = setInterval(() => {
-        // Update the text content of the loading indicator
+     
         element.textContent += '.';
 
-        // If the loading indicator has reached three dots, reset it
+        
         if (element.textContent === '....') {
             element.textContent = '';
         }
